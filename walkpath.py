d9b424de205b828028fad00537e2a79ebe2ffb1f
snovis <scottnovis@gmail.com>
2022-10-27 09:56:20 -0700

    Remove other examples, focus on glob.iglob

--- walkpath.py
+++ walkpath.py
@@ -10,37 +10,6 @@ from pathlib import Path
 # assign directory
 directory = 'E:\\NovisVault'
  
-# iterate over files in
-# that directory
-print("\n","=" * 80)
-print(" Use Path().glob()\n")
-files = Path(directory).glob('*.md')
-for file in files:
-    print(file)
-
-
-# try the os.scandir method 
-# iterate over files in
-# that directory
-print("\n","=" * 80)
-print(" Use Scandir\n")
-for filename in os.scandir(directory):
-    if filename.is_file():
-        print(filename.path)
-
-
-print("\n","=" * 80)
-print(" Use os.walk\n")
-
-# iterate over files in
-# that directory
-# for root, dirs, files in os.walk(directory):
-#     for filename in files:
-#         print(os.path.join(root, filename))
-
-
-        # import required module
- 
 
 print("\n","=" * 80)
 print(" Use glob.iglob\n")
